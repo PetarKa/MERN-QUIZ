458586f3eef2a021b4543adff5a71006f1247cab
PetarKa <karoglan.petar@gmail.com>
2022-09-05 12:00:25 +0200

    Basic login izgled

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './Login/FrontPage'
+import FrontPage from './Login/FrontPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
